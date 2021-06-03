84e020c89dc4675188f2a97ca5f52f342378b9f3
Renato Campelo <renalexster@gmail.com>
2021-06-02 22:28:48 -0400

    JobScheduler table

--- src/main/resources/db/migration/V20210602215411__scheduler_job_table.sql
+++ src/main/resources/db/migration/V20210602215411__scheduler_job_table.sql
@@ -6,3 +6,6 @@ create table public.job_scheduler
     executor_class  varchar not null,
     cron_exp        varchar not null
 );
+
+insert into public.job_scheduler(job_name, job_description, executor_class, cron_exp)
+VALUES ('Hello_Processor', 'Hello World Scheduler', 'com.example.scheduler.processor.HelloProcessor', '* * * * * ?');
